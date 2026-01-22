95a32f9b1c7c871103ab1d2416a6329b9530d25b
Adalberto Santos <adalbertoliveira@gmail.com>
2026-01-22 17:49:45 -0300

    Add MLflow model registry integration

---
+++ models/registry/mlflow_registry.py
@@ -0,0 +1,12 @@
+"""MLflow model registry integration."""
+
+class MLflowRegistry:
+    """Manage model versions in MLflow."""
+    
+    def register_model(self, model, name, tags):
+        """Register a new model version."""
+        pass
+    
+    def promote_model(self, name, version, stage):
+        """Promote model to production stage."""
+        pass
